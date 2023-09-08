29995a31c08932627687960a54719f673c5e48d7
alison alves <alisonribeiro075@gmail.com>
2023-09-08 14:58:29 -0300

    Testando variavel de ambiente vercel

--- src/pages/Contacts/useContact.js
+++ src/pages/Contacts/useContact.js
@@ -38,7 +38,12 @@ export default function useContact(){
       email: email,
       subject: assunto
     }
-
+    const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+    const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+    const userId = process.env.REACT_APP_EMAILJS_USER_ID;
+    console.log("Service ID:", serviceId);
+    console.log("Template ID:", templateId);
+    console.log("User ID:", userId);
 
     emailjs.send( 
       process.env.REACT_APP_EMAILJS_SERVICE_ID,
